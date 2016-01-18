56ca72e76d88f2c40e8622ee7b0aa77be1a9f09e
Lily Lemmer <lily.lemmer@ave81.com>
2016-01-17 22:25:28 -0600

    Less annoying style and MP3 config
    You can set the MP3 in `config.py`, if it is blank,
    the HTML5 player won't show up.
    
    Edited the stylesheet to be less annoying.

--- config.py
+++ config.py
@@ -1,6 +1,14 @@
 DATABASE = '/tmp/staticfuzz.db'
 DEBUG = True
 SECRET_KEY = 'BETTER CHANGE ME'
+
+# God/admin
 USERNAME = 'lain'
 PASSWORD = 'bear'
+
+# Maximum number of characters for memories.
 MAX_CHARACTERS = 140
+
+# If this is enabled an HTML audio player will
+# appear in the footer and loop an MP3.
+# BACKGROUND_MP3 = '/static/background.mp3'
