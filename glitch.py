c2152c76bb0ce8aed0e5a6268f147a5ae65cd238
Lily Lemmer <lily.lemmer@ave81.com>
2016-01-21 02:51:44 -0600

    glitch enhancements

---
+++ glitch.py
@@ -0,0 +1,77 @@
+"""Glitch an image and make it look all cool. :3
+
+"""
+
+import io
+import urllib2
+import random
+import base64
+
+from flask import current_app as app
+from PIL import Image, ImageOps
+from cStringIO import StringIO
+
+
+def glitch_from_url(url_string):
+    # get the image from the net
+    urlopen_result = urllib2.urlopen(url_string)
+    urlopen_result_io = io.BytesIO(urlopen_result.read())
+
+    # open and tweak the image
+    # open, resize...
+    tweaked_image = Image.open(urlopen_result_io)
+    tweaked_image.thumbnail([app.config['THUMB_MAX_WIDTH'],
+                             app.config['THUMB_MAX_HEIGHT']])
+
+    # save as low quality jpg
+    tweaked_image_io = StringIO()
+    tweaked_image.save(tweaked_image_io, format="JPEG", quality=10)
+    tweaked_image = Image.open(tweaked_image_io)
+
+    # autocontrast
+    tweaked_image = ImageOps.autocontrast(tweaked_image)
+    tweaked_image = ImageOps.equalize(tweaked_image)
+
+    # solarize
+    tweaked_image = ImageOps.solarize(tweaked_image, random.randint(1, 200))
+
+    # random chance to flip
+    if random.randint(0, 4):
+        tweaked_image = ImageOps.mirror(tweaked_image)
+
+    if random.randint(0, 4):
+        tweaked_image = ImageOps.equalize(tweaked_image)
+
+    # random chance to invert
+    if random.randint(0, 2):
+        tweaked_image = ImageOps.invert(tweaked_image)
+
+    max_colors = random.randint(app.config['MIN_COLORS'],
+    app.config['MAX_COLORS'])
+    tweaked_image = tweaked_image.convert(mode='P',
+                                          palette=Image.ADAPTIVE,
+                                          colors=max_colors)
+
+    # save the image as base64 HTML image
+    glitch_image = StringIO()
+    tweaked_image.save(glitch_image, "PNG", optimize=True)
+    glitch_string = glitch_image.getvalue()
+
+    # glitch right before encoding
+    for i in range(1, random.randint(2, 3)):
+        start_point = random.randint(len(glitch_string) / 2, len(glitch_string))
+        end_point = start_point + random.randint(0, len(glitch_string) - start_point)
+        section = glitch_string[start_point:end_point]
+
+        repeated = ''
+
+        for i in range(1, random.randint(1, 5)):
+            repeated += section
+
+    new_start_point = random.randint(2500, len(glitch_string))
+    new_end_point = new_start_point + random.randint(0, len(glitch_string) - new_start_point)
+    glitch_string = glitch_string[:new_start_point] + repeated + glitch_string[new_end_point:]
+
+    base64_string = base64.b64encode(glitch_string)
+
+    return base64_string
--- staticfuzz.py
+++ staticfuzz.py
@@ -17,20 +17,16 @@ Options:
 """
 
 
-import io
 import json
+import glitch
 import docopt
-import random
 import base64
 import gevent
 import sqlite3
-import urllib2
 import mimetypes
 
 from flask import *
 from flask_sqlalchemy import SQLAlchemy
-from PIL import Image, ImageOps
-from cStringIO import StringIO
 from contextlib import closing
 from gevent.pywsgi import WSGIServer
 from gevent import monkey
@@ -56,7 +52,7 @@ class Memory(db.Model):
         mimetype, __ = mimetypes.guess_type(text)
 
         if mimetype and mimetype.startswith(u'image'):
-            self.base64_image = glitch_from_url(text)
+            self.base64_image = glitch.glitch_from_url(text)
         else:
             self.base64_image = None
 
@@ -179,60 +175,6 @@ def logout():
     flash('God goes "bye!"')
 
     return redirect(url_for('show_memories'))
-
-
-def glitch_from_url(url_string):
-    # get the image from the net
-    urlopen_result = urllib2.urlopen(url_string)
-    urlopen_result_io = io.BytesIO(urlopen_result.read())
-
-    # open and tweak the image
-    # open, resize...
-    tweaked_image = Image.open(urlopen_result_io)
-    tweaked_image.thumbnail([app.config['THUMB_MAX_WIDTH'],
-                             app.config['THUMB_MAX_HEIGHT']])
-
-    # save as low quality jpg
-    tweaked_image_io = StringIO()
-    tweaked_image.save(tweaked_image_io, format="JPEG", quality=10)
-    tweaked_image = Image.open(tweaked_image_io)
-
-    # autocontrast
-    tweaked_image = ImageOps.autocontrast(tweaked_image)
-
-    # random chance to invert
-    if random.randint(0, 2):
-        tweaked_image = ImageOps.invert(tweaked_image)
-
-    max_colors = random.randint(app.config['MIN_COLORS'],
-                                app.config['MAX_COLORS'])
-    tweaked_image = tweaked_image.convert(mode='P',
-                                          palette=Image.ADAPTIVE,
-                                          colors=max_colors)
-
-    # save the image as base64 HTML image
-    glitch_image = StringIO()
-    tweaked_image.save(glitch_image, "PNG", optimize=True)
-    glitch_string = glitch_image.getvalue()
-
-    # glitch right before encoding
-    for i in range(1, random.randint(2, 3)):
-        start_point = random.randint(len(glitch_string) / 2, len(glitch_string))
-        end_point = start_point + random.randint(0, len(glitch_string) - start_point)
-        section = glitch_string[start_point:end_point]
-
-        repeated = ''
-
-        for i in range(1, random.randint(1, 5)):
-            repeated += section
-
-    new_start_point = random.randint(2500, len(glitch_string))
-    new_end_point = new_start_point + random.randint(0, len(glitch_string) - new_start_point)
-    glitch_string = glitch_string[:new_start_point] + repeated + glitch_string[new_end_point:]
-
-    base64_string = base64.b64encode(glitch_string)
-
-    return base64_string
 
 
 @app.route('/new_memory', methods=['POST'])
