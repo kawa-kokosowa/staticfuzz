f746dbfc7764b95e47346df3125c1cbb20409220
Lily Lemmer <lily.lemmer@ave81.com>
2016-01-21 00:21:41 -0600

    way better glitches

--- staticfuzz.py
+++ staticfuzz.py
@@ -187,29 +187,37 @@ def glitch_from_url(url_string):
     urlopen_result_io = io.BytesIO(urlopen_result.read())
 
     # open and tweak the image
+    # open, resize...
     tweaked_image = Image.open(urlopen_result_io)
-    tweaked_image.thumbnail([app.config['THUMB_MAX_WIDTH'], app.config['THUMB_MAX_HEIGHT']])
+    tweaked_image.thumbnail([app.config['THUMB_MAX_WIDTH'],
+                             app.config['THUMB_MAX_HEIGHT']])
+
+    # save as low quality jpg
+    tweaked_image_io = StringIO()
+    tweaked_image.save(tweaked_image_io, format="JPEG", quality=20)
+    tweaked_image = Image.open(tweaked_image_io)
 
     # autocontrast
-    tweaked_image = ImageOps.invert(tweaked_image)
+    tweaked_image = ImageOps.autocontrast(tweaked_image)
 
     # random chance to invert
     if random.randint(0, 2):
         tweaked_image = ImageOps.invert(tweaked_image)
 
-    tweaked_image = tweaked_image.convert(mode='P', palette=Image.ADAPTIVE,
-                                          colors=random.randint(app.config['MIN_COLORS'],
-                                                                app.config['MAX_COLORS']))
+    max_colors = random.randint(app.config['MIN_COLORS'],
+                                app.config['MAX_COLORS'])
+    tweaked_image = tweaked_image.convert(mode='P',
+                                          palette=Image.ADAPTIVE,
+                                          colors=max_colors)
 
     # save the image as base64 HTML image
     glitch_image = StringIO()
-    tweaked_image.save(glitch_image, "PNG")
+    tweaked_image.save(glitch_image, "PNG", optimize=True)
     glitch_string = glitch_image.getvalue()
 
     # glitch right before encoding
-    for i in range(1, random.randint(2, 4)):
-        # splice
-        start_point = random.randint(2500, len(glitch_string))
+    for i in range(1, random.randint(2, 3)):
+        start_point = random.randint(len(glitch_string) / 2, len(glitch_string))
         end_point = start_point + random.randint(0, len(glitch_string) - start_point)
         section = glitch_string[start_point:end_point]
 
