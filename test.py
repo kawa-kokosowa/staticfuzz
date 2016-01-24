9af3054c07f3b8fcc08b84d067b18b547ddc428a
Lily Lemmer <lily.lemmer@ave81.com>
2016-01-24 02:54:38 -0600

    Start unit tests

---
+++ test.py
@@ -0,0 +1,19 @@
+import pytest
+import staticfuzz
+
+
+@pytest.fixture
+def app():
+
+    return staticfuzz.app
+
+
+def test_index_route(client):
+    resp = client.get('/')
+    assert resp.status_code == 200
+
+
+def test_new_memory(client):
+    resp = client.post('/new_memory', data={'text': u'/danbooru goo_girl'},
+                       follow_redirects=True)
+    assert resp.status_code == 200
