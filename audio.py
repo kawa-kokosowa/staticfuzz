7b07ce888c346b9a3849fa182edc285bfeb1daee
Lily Lemmer <lily.lemmer@ave81.com>
2016-01-24 23:46:26 -0600

    better aduio

--- audio.py
+++ audio.py
@@ -23,7 +23,7 @@ def glitch_audio(url_string):
     sound = pydub.AudioSegment.from_file(urlopen_result_io, format="wav")
 
     # Glitch
-    for n in range(1, 10):
+    for n in range(1, 4):
         ms = random.randint(500, 1000)
 
         end_start = random.randint(ms, len(sound) - ms)
@@ -41,15 +41,19 @@ def glitch_audio(url_string):
             if range(0, 1):
                 segment = segment.reverse()
 
-            if range(0, 1):
+            if not range(0, 4):
                 segment = segment.fade_out(ms)
 
-            if range(0, 1):
+            if not range(0, 4):
                 segment = segment.fade_in(ms)
+
+            if not range(0, 6):
+                segment = segment + segment
 
             new_segments.append(segment)
 
         # chance to make segment backwards
+        random.shuffle(new_segments)
         sound = new_segments[0] + new_segments[1] + new_segments[2]
 
     # Save
